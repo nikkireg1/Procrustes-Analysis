ab28b1afdf72e730a6757a7f93a034e70b625f45
Nicole Regimbal <93806707+nikkireg1@users.noreply.github.com>
2023-06-22 18:29:38 -0400

    Add files via upload

---
+++ Procrustes Rotation Tutorial - Comparing Sites.R
@@ -0,0 +1,48 @@
+
+library(ggplot2) #load in the needed libraries
+library(vegan)
+
+getwd()
+setwd("C:/Users/19082/OneDrive") #set working directory
+dir()
+
+#Comparing two PCA's 
+#Comparing inventory/mortality for same species at the same sites
+#Are the species present and experiencing mortality the same for each site?
+
+#Inventory PCA
+inv <- read.csv("inventory_data.csv", row.names = 1)
+inv <- na.omit(inv)
+inv.pca <- rda(inv, scale=TRUE)
+summary(inv.pca)
+
+#Mortality PCA
+mort <- read.csv("mort_data.csv", row.names = 1)
+mort <- na.omit(mort)
+mort.pca <- rda(mort, scale=TRUE)
+summary(mort.pca)
+
+
+#Biplots for species mortality
+par(mfrow=c(1,2))
+biplot(mort.pca, scaling =1, type="text",  xlab = "PC1 (45.8%)", ylab = "PC2 (17.4%)")
+biplot(inv.pca, scaling =1,type="text", xlab = "PC1 (30.1%)", ylab = "PC2 (21.6%)")
+
+
+#Inventory and Mortality Procrustes Rotation
+proc <- procrustes(inv.pca, mort.pca, scale=TRUE)
+
+par(mfrow=c(1,1))
+plot(proc)
+text(
+  proc,
+  display = "target",
+  col = "red",
+  pos = 4,
+  cex=0.6
+)
+
+
+#Significance test of Procrustes
+protest <- protest(X=inv.pca, Y=mort.pca, scores="sites", permutations=999)
+protest
